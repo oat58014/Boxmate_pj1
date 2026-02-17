5c351ffd3907fb0502e85f114d09ca9d99430ba0
Chayanon Klomchaikhow <chayanon.klo@dome.tu.ac.th>
2026-02-17 23:01:06 +0700

    update link

--- src/component/UserProfile.jsx
+++ src/component/UserProfile.jsx
@@ -7,8 +7,8 @@ export default function UserProfile() {
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
 
   const externalLinks = [
-    { id: "tiktok", label: "TikTok", href: "https://www.tiktok.com/", icon: Video },
-    { id: "lazada", label: "Lazada", href: "https://www.lazada.co.th/", icon: ShoppingCart },
+    { id: "tiktok", label: "TikTok", href: "https://0171-124-122-9-179.ngrok-free.app/api/auth/tiktok", icon: Video },
+    { id: "lazada", label: "Lazada", href: "https://0171-124-122-9-179.ngrok-free.app/api/auth/lazada", icon: ShoppingCart },
     { id: "shopee", label: "Shopee", href: "https://shopee.co.th/", icon: ShoppingCart },
   ];
 
